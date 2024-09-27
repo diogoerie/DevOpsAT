9b2dfd023a840f67ceca6716212a03172dca110e
diogoerie <119828335+diogoerie@users.noreply.github.com>
2024-09-26 22:34:09 -0300

    Delete LojaDosCarros directory

--- LojaDosCarros/Veiculos/src/main/java/com/example/veiculos/repository/VeiculoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.veiculos.repository;
-
-import com.example.veiculos.entity.Veiculo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VeiculoRepository extends JpaRepository<Veiculo, Long> {
-}
